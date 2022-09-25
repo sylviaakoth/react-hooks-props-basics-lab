6db67f8645b13e1b0b0cfdd4e3815a5048ff9f36
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-09-25 19:01:18 +0300

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,13 @@ import user from "../data/user";
 console.log(user);
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
-  );
+	return (
+		<div>
+			<NavBar />
+			<Home name={user.name} color={user.color} city={user.city} />
+			<About bio={user.bio} links={user.links} />
+		</div>
+	);
 }
 
 export default App;
